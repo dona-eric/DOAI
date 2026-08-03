1165659f154183bac133a3cd330a80626f7ea4f6
dona-eric <donaerickoulodji@gmail.com>
2026-08-03 14:53:18 +0100

    feat: configuration des variables d'environnement , des clés api

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+import os
+import pathlib
+import requests
+import dotenv
+
+
+### chargement des données keys de l'environnement virtual
+dotenv.load_dotenv()
+
+# KEYS LLAMA CLOUD
+
+LLAMA_PARSE_API_KEY = os.getenv("LLAMA_PARSE_API_KEY")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+QDRANT_URL = "https://9825a19f-edf3-46a3-b7b7-cbe118083f9e.us-west-1-0.aws.cloud.qdrant.io"
