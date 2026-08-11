de9cd7b9c74903bbc291e5ea88ac66dd752780c2
dona-eric <donaerickoulodji@gmail.com>
2026-08-11 10:33:43 +0100

    feat: fonction principale

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 def main():
-    print("Welcome on !")
+    from transformers import pipeline
+    
+    
+    generator = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-360M")
+    prompt = """
+    In this course, we will teach you how to"""
+
+    result = generator(
+        prompt=prompt,
+        max_length=30,
+        num_return_sequences=2,
+    )
+    print(result[0]['generated_text'])
 
 
 if __name__ == "__main__":
