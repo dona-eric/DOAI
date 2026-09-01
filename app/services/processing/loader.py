7facfa483a7379b15520c3a41706e5cdb709d352
dona-eric <donaerickoulodji@gmail.com>
2026-09-01 13:32:55 +0100

    feat: chargement des documents prets pour les etapes supp de segmentation et de traitement vers database qdrant

--- app/services/processing/loader.py
+++ app/services/processing/loader.py
@@ -1,13 +1,8 @@
 import sys
 from pathlib import Path
-import os
-from typing import List
 from app.core.loger import setup_logging
 
-logger = setup_logging()
-
-logger.info("CHARGEMENT DES DOCUMENTS-ARTICLES-LAWER-CODE (PDF, DOCX)")
-
+logger = setup_logging(__name__)
 
 data = Path(__file__).resolve().parents[3] / "data"
 
@@ -16,15 +11,7 @@ def get_files():
     logger.info("Initialisation de la recherche des fichiers... ")
     files = []
 
-    for ext in (".pdf", ".docx", ".txt", ".md", ".xlsx", ".png", ".jpg"):
+    for ext in (".pdf", ".docx", ".txt", ".md", ".xlsx"):
         files.extend(list(data.rglob(f"*{ext}")))
 
     return sorted(files)
-
-if __name__=="__main__":
-    files = get_files()
-    logger.info(f"Nombres de fichiers trouvés: {len(files)}")
-    logger.info("Fichiers trouvés: ")
-    logger.info(files)
-
-        
